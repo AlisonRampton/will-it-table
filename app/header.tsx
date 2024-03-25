81da9bcda32db27d783e34e80b2b7ea3f8f2224d
Alison Rampton <alison.rampton@gmail.com>
2024-03-25 14:46:55 -0600

    Fleshed out header, added search bar

--- app/header.tsx
+++ app/header.tsx
@@ -1,15 +1,22 @@
+import Search from "./search";
+
 export default function Header({ bigMobile }: { bigMobile: boolean }) {
   return (
     <div
       className={
-        `w-full p-2` + bigMobile ? `flex flex-col md:flex-row` : `flex-row`
+        `w-full p-2 ` +
+        (bigMobile ? `flex flex-col md:flex-row` : `flex flex-row`) +
+        ` items-center justify-between`
       }
     >
       <div className="flex flex-row items-center">
         <img src="will-it-table-logo.png" className="w-16 md:w-24" />
         <h1 className="pl-8 text-2xl md:text-4xl">Will It Table?</h1>
       </div>
-      {/* search bar */}
+
+      <div>
+        <Search placeholder="Search" />
+      </div>
     </div>
   );
 }
