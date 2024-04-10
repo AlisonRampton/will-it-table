8b2ff5e00252eca782faa270c6bd2ffe7dd1b772
Alison Rampton <alison.rampton@gmail.com>
2024-04-10 12:07:48 -0600

    changed logo, fixed margin issues on cards

--- app/cardsColumns.tsx
+++ app/cardsColumns.tsx
@@ -20,7 +20,10 @@ const CardsColumns: React.FC<CardsColumnsProps> = ({ boardGames }) => {
   return (
     <div className="sm:columns-2 lg:columns-3 xl:columns-4 justify-center">
       {boardGames.map((game) => (
-        <div key={game.id} className="m-4 break-inside-avoid-column">
+        <div
+          key={game.id}
+          className="mt-4 mr-4 ml-4 mb-4 first:mt-0 last:mb-0 break-inside-avoid-column"
+        >
           <BoardGame
             name={game.name}
             thumbnail={game.thumbnail}
